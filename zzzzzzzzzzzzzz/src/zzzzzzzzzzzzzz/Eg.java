e99b5566063a0eb7f3b41f98fc8da3f99936f9ee
Su <Su@DESKTOP-UHNMMSV>
2019-10-24 12:05:36 +0530

    initial code

---
+++ zzzzzzzzzzzzzz/src/zzzzzzzzzzzzzz/Eg.java
@@ -0,0 +1,12 @@
+package zzzzzzzzzzzzzz;
+
+public class Eg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is Git example");
+
+	}
+
+}
